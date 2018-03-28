1ab45fa7908dd0cdf8693d7236739e37dece0f0a
Kristo Roots <rootkris@users.noreply.github.com>
2018-03-28 14:32:26 +0300

    prototype

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 /* TYPER */
-var night;
 const TYPER = function () {
   if (TYPER.instance_) {
     return TYPER.instance_
@@ -15,23 +14,12 @@ const TYPER = function () {
   this.word = null
   this.wordMinLength = 5
   this.guessedWords = 0
+  this.score = 0
 
-  this.init()  
-  
+  //this.init()
 }
 
 window.TYPER = TYPER
-
-function nightMode() {
-  if (night === 1) {
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
-    night = 0;
-  } else {
-    document.body.style.backgroundColor = "rgb(75, 75, 75)";
-    night = 1;
-  }
-}
 
 TYPER.prototype = {
   init: function () {
@@ -70,16 +58,11 @@ TYPER.prototype = {
     this.word.Draw()
 
     window.addEventListener('keypress', this.keyPressed.bind(this))
-	this.startTime = new Date().getTime()
-	window.setInterval(this.loop.bind(this), 1)
+		this.startTime = new Date().getTime()
+		window.setInterval(this.loop.bind(this),1)
   },
-  
-  loop: function() {
-	  this.word.Draw()
-	  const currentTime = new Date().getTime()
-	  this.counter = currentTime - this.startTime
-  },  
 
+  
   generateWord: function () {
     const generatedWordLength = this.wordMinLength + parseInt(this.guessedWords / 5)
     const randomIndex = (Math.random() * (this.words[generatedWordLength].length - 1)).toFixed()
@@ -88,7 +71,6 @@ TYPER.prototype = {
     this.word = new Word(wordFromArray, this.canvas, this.ctx)
   },
 
-  
   keyPressed: function (event) {
     const letter = String.fromCharCode(event.which)
 
@@ -97,12 +79,11 @@ TYPER.prototype = {
 
       if (this.word.left.length === 0) {
         this.guessedWords += 1
-		if (this.guessedWords == 5){
-			this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-			gameFinish();
-		}
-		
-		
+		this.score += typer.word.word.length * 2
+			if(this.guessedWords == 10){
+				this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+				gameOver();
+			}	
         this.generateWord()
       }
 
@@ -124,17 +105,40 @@ Word.prototype = {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
     this.ctx.textAlign = 'center'
-    this.ctx.font = '140px Courier'
+    this.ctx.font = '140px Century Gothic'
     this.ctx.fillText(this.left, this.canvas.width / 2, this.canvas.height / 2)
 	
-	this.ctx.textAlign = 'center'
-    this.ctx.font = '140px Arial'
-    this.ctx.fillText(typer.counter, 300, 300)
+	this.ctx.textAlign = "left"
+	this.ctx.font = "50px Century Gothic"
+	this.ctx.fillText("Score:" + typer.score, 250, 250)
 	
-	this.ctx.textAlign = 'center'
-    this.ctx.font = '140px Arial'
-    this.ctx.fillText(typer.guessedWords, 500, 500)
-  },//hetkel kuvab nii kulunud aega kui ka skoori
+	this.ctx.textAlign = "left"
+	this.ctx.font = "50px Century Gothic"
+	this.ctx.fillText("Guessed words:" + typer.guessedWords, 250, 300)
+  },
+  
+  infoScreen: function(){
+	this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+	
+	this.ctx.textAlign = "center"
+	this.ctx.font = "140px Century Gothic"
+	this.ctx.fillText("Game over!", this.canvas.width / 2, this.canvas.height / 2 - this.canvas.height / 10)
+	
+	this.ctx.font = "100px Century Gothic"
+	this.ctx.fillText("Score: " + typer.score, this.canvas.width / 2, this.canvas.height-this.canvas.height / 1.5)
+	
+	for (let i = 0, len = JSON.parse(localStorage.getItem('arr')).length; i < len; i++) {
+           if (i < 10) {
+			this.ctx.textAlign = 'right'
+			this.ctx.font = '80px Century Gothic'
+			this.ctx.fillText("Nr" + (i+1) + ")      " +
+				"Name: " + JSON.parse(localStorage.getItem('arr'))[i][0] +
+				"   " +
+				"Score: " + JSON.parse(localStorage.getItem('arr'))[i][1],
+				this.canvas.width / 2, 200 + 100*i)
+            }
+        }
+   },
 
   removeFirstLetter: function () {
     this.left = this.left.slice(1)
@@ -155,31 +159,52 @@ function structureArrayByWordLength (words) {
   return tempArray
 }
 
-function startGame(){
-	const typer = new TYPER()
-	window.typer = typer
-}
-
-function restartGame(){
-	this.guessedWords = 0
-	const typer = new TYPER()
-	window.typer = typer
-	typer.generateWord()
-	typer.word.Draw()
-
-}
-
-function gameFinish(){
-	let r = confirm("Game over! \n Your Score: " + "\n Again?")
-	if(r == true){
-		location.reload()
-	}
-	else {
-		location.href = 'first.html';
-	}
-}
-
 window.onload = function () {
   const typer = new TYPER()
   window.typer = typer
+}
+
+function gameOver(){
+	typer.word.infoScreen()
+	saveData(name,typer.score)
+}
+
+
+function saveData (playerName, playerScore) {
+    arr = []
+    if (window.localStorage.length == 0) {
+        player = [playerName,playerScore]
+        arr.push(player)
+        localStorage.setItem('arr', JSON.stringify(arr))
+    } else {
+        let stored = JSON.parse(localStorage.getItem('arr'))
+        let player2 = [playerName,playerScore]
+        stored.push(player2)
+
+        //sort
+        let length = stored.length
+        for (let i=0; i<length; i++) {
+            for (let j=0; j<(length-i-1); j++) {
+                if (stored[j][1] < stored[j+1][1]) {
+                    let tmp = stored[j]
+                    stored[j] = stored[j+1]
+                    stored[j+1] = tmp
+                }
+            }
+        }
+
+        localStorage.setItem('arr', JSON.stringify(stored))
+    }
+}
+
+function playGame(){
+	if(document.querySelector("#name").value !=""){
+		name = document.querySelector("#name").value
+		document.querySelector("body").innerHTML = "<canvas></canvas>"
+		const typer = new TYPER()
+		window.typer = typer
+		typer.init()
+	} else {
+		alert("Please enter name!")
+	}
 }
